a4b1304e40bd899674ef46df3cf715fc0e19281e
Dolinin2021 <programmistdolinin@mail.ru>
2024-03-20 17:51:14 +0600

    modified settings.py and requirements.txt

--- diplom_project/diplom_project/settings.py
+++ diplom_project/diplom_project/settings.py
@@ -30,15 +30,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = 'django-insecure-k=s2)im1!gsa0j4ocu7x#q$z66_@cffqexux-0zroo(b5k4tu2'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = True
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
@@ -142,17 +139,6 @@ WSGI_APPLICATION = 'diplom_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'diplom_database',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres'
-#     }
-# }
 
 DATABASES = {
     'default': {
